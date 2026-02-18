bbed12ca85c831dda1875e61a6008cabb40e19c9
Piyush <144081056+Piyush-Rajbanshi@users.noreply.github.com>
2026-02-19 03:20:40 +0545

    Update test_connection.py

--- test_connection.py
+++ test_connection.py
@@ -2,7 +2,7 @@ from db_connection import get_connection
 
 try:
     conn = get_connection()
-    print("✅ Database connected successfully")
+    print("Database connected successfully")
     conn.close()
 except Exception as e:
-    print("❌ Error:", e)
+    print("Error:", e)
